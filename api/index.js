1525799d46e48b3262b94803908dee3672edafc7
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2024-03-16 15:21:03 +0000

    established database connection

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 import Koa from "koa";
 import dotenv from "dotenv";
+import { connectToDatabase } from "./mongo/index.js";
 
+// Load environment variables
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+// Creating new Koa instance
 const app = new Koa();
 
 app.use(async (ctx) => {
   ctx.body = "Test App";
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Database connection and server start
+connectToDatabase().then(() => {
+  app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+  });
 });
